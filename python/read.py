80aa8ae9eeced84551e8ec8710ebea528d2cc70c
psatomas <psatomas@gmail.com>
2024-11-13 19:20:45 -0300

    feat: add class.py and write.py

--- python/read.py
+++ python/read.py
@@ -1,4 +1,4 @@
-coun_file = open("c:/Users/TOMAS/Documents/Projects/python/countries.txt", "r")
+coun_file = open("c:/Users/TOMAS/Documents/Projects/python/text.txt", "r")
 for lines in coun_file.readlines():
     print(lines)
 coun_file.close()
---
+++ python/write.py
@@ -0,0 +1,2 @@
+coun_file = open("c:/Users/TOMAS/Documents/Projects/python/new.py", "w")
+coun_file.write('print(\'This is a new file\')')
